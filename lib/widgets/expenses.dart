6ffe373d26af8ebf7ea8817597c711b5f4cf5cf0
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-03-21 21:55:10 -0600

    adding modal sheet + inputs with buttons

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,6 @@
+import 'package:expence_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
+
 import 'package:expence_tracker/models/expense.dart';
 import 'package:expence_tracker/widgets/expenses_list/expenses_list.dart';
 
@@ -27,11 +29,28 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
+          // Toolbar
           const Text('The chart'),
           Expanded(
             child: ExpensesList(expenses: _registeredExpenses),
